36cb2d7d3395a1fc5951c0c95e80b00fb584bf25
Angelina Yessyca Rahardjo Hadi <90541566+01Angel01@users.noreply.github.com>
2023-05-20 21:06:01 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,14 +7,30 @@ public class Driver {
 		Counter counter = new Counter();
 		
 		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Count saat ini:" + counter.getCount());
 		
-		counter.increment();
+		counter.increaseBy(1);
+		counter.increaseBy(2);
+		counter.increaseBy(3);
+		counter.increaseBy(4);
+		counter.increaseBy(5);
 		
 		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Count saat ini:" + counter.getCount());
 		
-		counter.decrement();
+		counter.decreaseBy(1);
+		counter.decreaseBy(2);
+		counter.decreaseBy(3);
 		
 		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Count saat ini:" + counter.getCount());
+
+		counter.multiplyBy(1);
+		counter.multiplyBy(2);
+		counter.multiplyBy(3);
+		counter.multiplyBy(4);	
+		counter.multiplyBy(5);
+		counter.multiplyBy(6);
 		
 	}
 
